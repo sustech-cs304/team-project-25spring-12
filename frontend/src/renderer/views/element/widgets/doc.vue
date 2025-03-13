1d589562814a7db1a2713fa7efd71832c2f897fa
sustechkl <12111012@mail.sustech.edu.cn>
2025-03-13 19:04:46 +0800

    fixbug

--- frontend/src/renderer/views/element/widgets/doc.vue
+++ frontend/src/renderer/views/element/widgets/doc.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import {computed, defineProps, ref} from "vue";
+import {computed, ref} from "vue";
 import widgetCard from "./widget-card.vue";
 import MdAndFile from "./md-and-file.vue";
 
