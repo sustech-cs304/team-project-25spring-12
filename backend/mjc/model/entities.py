7ac10c35d740d5a7349cb9b03489f795e077a878
IceLocke <lockeice@gmail.com>
2025-03-21 15:39:53 +0800

    完成 Class-Folder-Page-Widget 设计 View 层逻辑的数据库结构设计

--- backend/mjc/model/entities.py
+++ backend/mjc/model/entities.py
@@ -1,4 +1,7 @@
-from sqlmodel import Field, SQLModel, Relationship
+from datetime import datetime
+from enum import Enum
+
+from sqlmodel import Field, SQLModel, Relationship, Enum as SQLEnum
 
 
 class User(SQLModel, table=True):
@@ -27,3 +30,143 @@ class Profile(SQLModel, table=True):
 
     username: str = Field(nullable=False, foreign_key="mjc_user.username")
     user: User = Relationship(back_populates="profile")
+
+
+class SyllabusFile(SQLModel, table=True):
+    """
+    用于管理系统中 Class 所使用的教学大纲的文件
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    file_name: str | None
+    url: str = Field(nullable=False)
+
+
+class Class(SQLModel, table=True):
+    """
+    课程
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False)
+    course_code: str = Field(nullable=False)
+    semester: str
+    lecturer: str
+    location: str
+    time: datetime
+    syllabus: str  # URL
+    # TODO: 教师、学生、助教 关系
+    # TODO: 文件夹、页面 关系
+
+
+class Folder(SQLModel, table=True):
+    """
+    课程页面集合
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    folder_name: str = Field(nullable=False, index=True)
+
+    pages: list["Page"] = Relationship(back_populates="folder")
+
+
+class Page(SQLModel, table=True):
+    """
+    页面，用于管理 Widgets
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False, index=True)
+    widgets: list["Widget"] = Relationship(back_populates="page")
+    folder_id: int | None = Field(default=None, foreign_key="mjc_folder.id")
+
+    folder: Folder = Relationship(back_populates="pages")
+
+
+class WidgetType(str, Enum):
+    note_pdf = 'notepdf'
+    doc = 'doc'
+    assignment = 'assignment'
+
+
+class WidgetAttachment(SQLModel, table=True):
+    """
+    用于管理系统中 Widget 使用的文件
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    file_name: str | None
+    url: str = Field(nullable=False)
+    upload_time: datetime | None
+    uploader_username: str | None = Field(nullable=False, foreign_key="profile.username")
+    widget_id: int = Field(foreign_key="widget.id")
+
+    uploader: "Profile" = Relationship()
+    widget: "Widget" = Relationship(back_populates="attachments")
+
+
+class Widget(SQLModel, table=True):
+    """
+    页面中的控件，可以自由组合
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    title: str | None
+    index: int = Field(nullable=False)
+    type: SQLEnum[WidgetType] = Field(nullable=False)
+    create_time: datetime | None
+    update_time: datetime | None
+    editor_username: str = Field(nullable=True, foreign_key="profile.username")
+    content: str | None
+    page_id: int = Field(foreign_key="page.id")
+
+    editor: "Profile" = Relationship()  # 此处通过 widget 不应能改变 profile，不加 back_populates
+    attachments: list[WidgetAttachment] = Relationship(back_populates="widget")
+    page: "Page" = Relationship(back_populates="widgets")
+
+    # 对应到具体的 Widget 内容
+    note_pdf_widget: "NotePDFWidget" = Relationship(back_populates="widget")
+    assignment_widget: "AssignmentWidget" = Relationship(back_populates="widget")
+
+
+
+class Note(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    page: int
+    x: int
+    y: int
+    text: str
+    editor_username: str = Field(nullable=True, foreign_key="profile.username")
+    note_pdf_widget_id: int = Field(foreign_key="note_pdf_widget.id")
+
+    editor: "Profile" = Relationship()  # 此处通过 note 不应能改变 profile，不加 back_populates
+    note_pdf_widget: "NotePDFWidget" = Relationship(back_populates="notes")
+
+
+class NotePDFWidget(SQLModel, table=True):
+    """
+    NotePDF Widget 存储的数据
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    widget_id: int = Field(foreign_key="widget.id")
+    pdf_file: str
+
+    widget: "Widget" = Relationship(back_populates="note_pdf_widget")
+    notes: list[Note] = Relationship(back_populates="note_pdf_widget")
+
+
+class SubmitTypes(str, Enum):
+    """
+    作业可选的提交类型
+    """
+    text = 'text'
+    file = 'file'
+    code = 'code'
+
+
+class AssignmentWidget(SQLModel, table=True):
+    """
+    Assignment Widget 存储的数据
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    widget_id: int = Field(foreign_key="widget.id")
+    submit_types: list[SQLEnum[SubmitTypes]] = Relationship(back_populates="widget")
+    ddl: datetime
+    max_score: int
+
+    widget: "Widget" = Relationship(back_populates="assignment_widget")
+    # TODO: 跟已提交的作业的关系
