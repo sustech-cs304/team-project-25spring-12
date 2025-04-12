638d6defdf81c5bfde42c56222db136aa5e2e53a
sustechkl <12111012@mail.sustech.edu.cn>
2025-04-12 22:37:50 +0800

    完成base request

---
+++ frontend/src/renderer/utils/request.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import humps from 'humps'
+
+const service = axios.create({
+    baseURL: '', // TODO: baseURL
+    timeout: 10000,
+})
+
+// 请求前：camelCase 转 snake_case
+service.interceptors.request.use(config => {
+    if (config.data) {
+        config.data = humps.decamelizeKeys(config.data)
+    }
+    if (config.params) {
+        config.params = humps.decamelizeKeys(config.params)
+    }
+    return config
+})
+
+// 响应后：snake_case 转 camelCase
+service.interceptors.response.use(
+    response => {
+        if (response.data) {
+            response.data = humps.camelizeKeys(response.data)
+        }
+        return response
+    },
+    error => Promise.reject(error)
+)
+
+export default service
