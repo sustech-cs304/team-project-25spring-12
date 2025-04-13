d79963aa6a348b7e8ae84827075e149969aaaa50
siyao <yusiyao821@gmail.com>
2025-04-13 16:11:18 +0800

    拆分crud，新增assignment

---
+++ backend/mjc/crud/assignment.py
@@ -0,0 +1,28 @@
+from sqlmodel import Session, select
+
+from backend.mjc.model.entity import SubmittedAssignment, Widget, SubmittedAssignmentFeedback
+
+
+def get_all_assignments_submissions(db: Session, widget_id: int) -> list[SubmittedAssignment]:
+    stmt = select(SubmittedAssignment).where(SubmittedAssignment.widget_id == widget_id).join(Widget).where(Widget.is_deleted == False)
+    submissions: list[SubmittedAssignment] = db.exec(stmt).all()
+    return submissions
+
+
+def get_user_assignment_submissions(db: Session, widget_id: int, username: str) -> list[SubmittedAssignment]:
+    stmt = select(SubmittedAssignment) \
+                .where(SubmittedAssignment.widget_id == widget_id) \
+                .where(SubmittedAssignment.username == username) \
+                .join(Widget) \
+                .where(Widget.is_deleted == False)
+    submissions: list[SubmittedAssignment] = db.exec(stmt).all()
+    return submissions
+
+
+def get_last_feedback(db: Session, widget_id: int, username: str) -> SubmittedAssignmentFeedback:
+    stmt = select(SubmittedAssignment) \
+            .where(SubmittedAssignment.widget_id == widget_id) \
+            .where(SubmittedAssignment.username == username) \
+            .order_by(SubmittedAssignment.feedback.create_time.desc()).limit(1)
+    feedback = db.exec(stmt).first().feedback
+    return feedback
