35ea3275d344ef53f5b18e8e44f6314c2871ccb9
IceLocke <lockeice@gmail.com>
2025-04-13 20:15:27 +0800

    添加获取单个submitted assignment的函数

--- backend/mjc/crud/assignment.py
+++ backend/mjc/crud/assignment.py
@@ -3,6 +3,12 @@ from sqlmodel import Session, select
 from backend.mjc.model.entity import SubmittedAssignment, Widget, SubmittedAssignmentFeedback
 
 
+def get_submitted_assignment(db: Session, assign_id: int) -> SubmittedAssignment:
+    stmt = select(SubmittedAssignment).where(SubmittedAssignment.id == assign_id)
+    assign: SubmittedAssignment = db.exec(stmt).first()
+    return assign
+
+
 def get_all_assignments_submissions(db: Session, widget_id: int) -> list[SubmittedAssignment]:
     stmt = select(SubmittedAssignment).where(SubmittedAssignment.widget_id == widget_id).join(Widget).where(Widget.is_deleted == False)
     submissions: list[SubmittedAssignment] = db.exec(stmt).all()
