9f50c1dab95adbac3090f736facd7819094988ef
siyao <yusiyao821@gmail.com>
2025-04-13 20:08:27 +0800

    添加assignment的router

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from redis import asyncio as aioredis
 
 from backend.mjc.utils.database import create_db_and_tables
 from backend.mjc.utils.keybuilder import request_key_builder
-from backend.mjc.router import user, course, page, widget, common
+from backend.mjc.router import user, course, page, widget, common, assignment
 
 
 @asynccontextmanager
@@ -35,4 +35,5 @@ app.include_router(course.router)
 app.include_router(page.router)
 app.include_router(widget.router)
 app.include_router(common.router)
+app.include_router(assignment.router)
 
