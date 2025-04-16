c3a5839891ac2073ef9ec020f4d2c4c2c2cfb6b3
sustechkl <12111012@mail.sustech.edu.cn>
2025-04-16 21:51:53 +0800

    默认权限改为公开

--- frontend/src/renderer/composables/useUploader.ts
+++ frontend/src/renderer/composables/useUploader.ts
@@ -5,7 +5,7 @@ export function useUploader() {
     const upload = async (file: File): Promise<FileMeta> => {
         const formData = new FormData();
         formData.append("file", file);
-        formData.append("visibility", "in_class");  // 之后可以区分权限
+        formData.append("visibility", "public");  // 之后可以区分权限
 
         const response = await uploadFile(formData);
         return response.data;
