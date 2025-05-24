b6fccee26edb6b9792a48617d4236e1f69222cd5
siyao <yusiyao821@gmail.com>
2025-05-24 17:44:37 +0800

    修改page鉴权的函数,删除无关引用

--- backend/mjc/permission/page.py
+++ backend/mjc/permission/page.py
@@ -6,21 +6,28 @@ from mjc.utils.database import SessionDep
 from mjc.model.schema.user import UserInDB
 from mjc.model.entity.course import ClassRole
 from mjc.service.user import get_current_user
-from mjc.service import page as page_service, course as course_service
-from mjc.model.schema.course import ClassUpdate
+from mjc.service import course as course_service
 from mjc.crud import page as crud_page
 
 
 def verify_page_exist(db: Session, page_id: int):
-    if crud_page.get_page(db, page_id) is None:
+    page = crud_page.get_page(db, page_id)
+    if page is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Page not found")
+    return page
+
+
+def verify_class_admin(db: Session, class_id: int, username: str):
+    role = course_service.get_user_class_role(db, username, class_id)
+    if role is None:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not in this class")
+    elif role is ClassRole.STUDENT:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You have not permission")
 
 
 async def verify_page_get(db: SessionDep, page_id: int,
                     current_user: UserInDB = Depends(get_current_user)):
-    page = crud_page.get_page(db, page_id)
-    if page is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Page not found")
+    page = verify_page_exist(db, page_id)
     role = course_service.get_user_class_role(db, current_user.username, page.class_id)
     if role is None:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not in this class")
@@ -32,34 +39,17 @@ async def verify_page_get(db: SessionDep, page_id: int,
 async def verify_page_create(db: SessionDep,
                        request: PageCreate,
                        current_user: UserInDB = Depends(get_current_user)):
-    role = course_service.get_user_class_role(db, current_user.username, request.class_id)
-    if role is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not in this class")
-    elif role is ClassRole.STUDENT:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You have not permission")
+    verify_class_admin(db, request.class_id, current_user.username)
 
 
 async def verify_page_update(db:SessionDep,
                        request: PageUpdate,
                        current_user: UserInDB = Depends(get_current_user)):
-    page = crud_page.get_page(db, request.page_id)
-    if page is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Page not found")
-    role = course_service.get_user_class_role(db, current_user.username, page.class_id)
-    if role is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not in this class")
-    elif role is ClassRole.STUDENT:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You have not permission")
+    verify_page_exist(db, request.id)
+    verify_class_admin(db, request.class_id, current_user.username)
 
 
 async def verify_page_delete(db: SessionDep, page_id: int,
                        current_user: UserInDB = Depends(get_current_user)):
-    verify_page_exist(db, page_id)
-    page = crud_page.get_page(db, page_id)
-    if page is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Page not found")
-    role = course_service.get_user_class_role(db, current_user.username, page.class_id)
-    if role is None:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not in this class")
-    elif role is ClassRole.STUDENT:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You have not permission")
+    page = verify_page_exist(db, page_id)
+    verify_class_admin(db, page.class_id, current_user.username)
