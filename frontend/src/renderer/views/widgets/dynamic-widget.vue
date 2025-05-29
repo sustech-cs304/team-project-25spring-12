d5c478614012a599af5494f6950154dd24b88d01
sustechkl <12111012@mail.sustech.edu.cn>
2025-05-29 23:14:02 +0800

    移出常量

--- frontend/src/renderer/views/widgets/dynamic-widget.vue
+++ frontend/src/renderer/views/widgets/dynamic-widget.vue
@@ -7,15 +7,31 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue'
-import {widgetMap} from '@/types/widgets'
+import {computed, DefineComponent, ref} from 'vue'
+import {WidgetType} from '@/types/widgets'
 import type {WidgetUnion} from '@/types/widgets'
+import NotePdf from '@/views/widgets/notepdf.vue'
+import Doc from '@/views/widgets/doc.vue'
+import Assignment from '@/views/widgets/assignment.vue'
 
 const props = defineProps<{
   data: WidgetUnion;
   canEdit: boolean;
 }>()
 
+const emit = defineEmits<{
+  (e: "update", data: WidgetUnion): void;
+}>();
+
+const handleUpdate = (data: WidgetUnion) => {
+  emit('update', data)
+}
+
+const widgetMap: Record<WidgetType, DefineComponent<any, any, any>> = {
+  notepdf: NotePdf,
+  doc: Doc,
+  assignment: Assignment,
+};
 const Component = computed(() => widgetMap[props.data.type]);
 
 const innerWidgetRef = ref<any>(null);
